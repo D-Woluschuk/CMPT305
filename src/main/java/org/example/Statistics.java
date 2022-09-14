87a895f1e5f131379f2ced00108f2104a7df5574
Dylan <woluschukd2@mymacewan.ca>
2022-09-14 16:27:17 -0600

    Implemented readCSV, some Record class, and a couple others

--- src/main/java/org/example/Statistics.java
+++ src/main/java/org/example/Statistics.java
@@ -1,4 +1,14 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Statistics {
+
+    public static int getNumberOfEntries(ArrayList<String> fileContents){
+        int entryCount;
+
+        entryCount = fileContents.size();
+
+        return entryCount;
+    }
 }
