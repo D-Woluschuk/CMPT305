a0593f5e5116450996f8f5f3bf0b0f0bfc6c7af2
Dylan <woluschukd2@mymacewan.ca>
2022-09-29 19:40:30 -0600

    Added RecordTest

---
+++ src/test/java/org/example/RecordTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecordTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+}
